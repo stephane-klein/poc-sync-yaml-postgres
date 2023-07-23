89451f0b8fc5dc781f550174ddb2da4bf7ed6bdb
Stéphane Klein <contact@stephane-klein.info>
2023-07-23 11:01:45 +0200

    Improve feeds-to-yaml.js output

--- feeds-to-yaml.js
+++ feeds-to-yaml.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 // import fs from 'fs';
 // import yaml from 'js-yaml';
+import { format } from 'date-fns'
 import nunjucks from 'nunjucks';
 import util from 'util';
 import pkg from "pg";
@@ -15,6 +16,8 @@ const pool = new Pool({
     port: process.env.POSTGRES_PORT || 5432
 });
 
+const db_version_datetime = (await pool.query(`SELECT version_datetime FROM sync_yaml_state.resource_states WHERE resource_name='feeds'`)).rows?.[0]?.version_datetime;
+
 const result = (await pool.query(`
     SELECT
         slug,
@@ -26,12 +29,24 @@ const result = (await pool.query(`
 `)).rows;
 // console.log(util.inspect(result, {showHidden: false, depth: null, colors: true}))
 
-nunjucks.configure({ autoescape: true });
-console.log(nunjucks.renderString(`
-{%- for item in result %}- slug: {{ item.slug }}
-  name: {{ item.name }}
-{% endfor -%}`,
-    {result: result}
-));
+nunjucks.configure({
+    autoescape: false,
+    trimBlocks: true,
+    lstripBlocks: true
+});
+console.log(nunjucks.renderString(
+    `
+version: {{ version }}
+feeds:
+  {% for item in result %}
+  - slug: {{ item.slug }}
+    name: {{ item.name }}
+  {% endfor -%}
+    `,
+    {
+        version: format(db_version_datetime, 'yyyy-mm-dd HH:MM:SS'),
+        result: result
+    }
+).trim());
 
 await pool.end();
